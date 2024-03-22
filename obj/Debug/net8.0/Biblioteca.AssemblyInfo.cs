ff38d25cf95cbd1c17e6b84eae928bec4b9e1654
Davidevk2 <k2guffy@gmail.com>
2024-03-22 18:57:28 -0500

    bases para las vistas de usuarios

--- obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+199ee38c550755e8e4f7f75c0bc34a83ae75346a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93da122812a028a3cc56ef33282768053eeb31b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
